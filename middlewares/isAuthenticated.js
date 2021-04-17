4053b3025c1e08f38449adddbbdab9ecbc9b8b9f
GiovanniCignetti <giovanni.cignetti@gmail.com>
2021-04-17 10:09:32 +0200

    update

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,13 +1,17 @@
+// import model User pour la recherche via le token
 const User = require("../models/User");
+
 const isAuthenticated = async (req, res, next) => {
-  // Récupérer le token
+  // Récupérer le token dans la requete
   const token = req.headers.authorization.replace("Bearer ", "");
+
   // Chercher le user qui possède ce token dans la BDD
   const user = await User.findOne({ token: token });
+
   if (user) {
     // ajouter à req l'objet user
     req.user = user;
-    // S'il exsite =====> next()
+    // poursuite du traitement dans la route
     next();
   } else {
     // Sinon ====> unauthorized
--- routes/offer.js
+++ routes/offer.js
@@ -7,10 +7,10 @@ require("dotenv").config();
 const app = express();
 app.use(formidable());
 
+const isAuthenticated = require("../middlewares/isAuthenticated");
+
 const Offer = require("../models/Offer");
 const User = require("../models/User");
-
-const isAuthenticated = require("../middlewares/isAuthenticated");
 
 router.post("/offer/publish", isAuthenticated, async (req, res) => {
   try {
@@ -27,8 +27,6 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
     } = req.fields;
 
     // création de l'annonce avant l'upload afin d'avoir l'id
-    const user = await User.findOne({ email: "giovanni@lereacteur.io" });
-
     const newOffer = new Offer({
       product_name: title,
       product_description: description,
@@ -50,39 +48,23 @@ router.post("/offer/publish", isAuthenticated, async (req, res) => {
           EMPLACEMENT: city,
         },
       ],
-      product_image: null,
-      owner: user,
+      // product_image: null,
+      owner: req.user,
     });
-    await newOffer.save();
+
+    // await newOffer.save();
 
     // Upload photo vers cloudinary
     const result = await cloudinary.uploader.upload(req.files.picture.path, {
-      folder: `/vinted/offers/${newOffer.id}/`,
+      folder: `/vinted/offers/${newOffer.id}`,
     });
 
     //mise à jour de l'offre
-    //user = await User.findOne({ email: "giovanni@lereacteur.io" });
     newOffer.product_image = result;
     await newOffer.save();
 
-    // Etape4 : répondre au client
-    res.status(200).json({
-      _id: newOffer.id,
-      product_name: newOffer.product_name,
-      product_description: newOffer.product_description,
-      product_price: newOffer.product_price,
-      product_details: newOffer.product_details,
-      owner: {
-        account: user.account,
-        _id: user.id,
-      },
-      product_image: {
-        // ...
-        // informations sur l'image du produit
-        secure_url: result.secure_url,
-        // ...
-      },
-    });
+    // réponse client
+    res.status(200).json(newOffer);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -92,7 +74,9 @@ router.get("/offers", async (req, res) => {
   try {
     const { title, priceMin, priceMax, sort, page } = req.query;
 
-    const filter = {};
+    // déclaration objet vide
+    let filter = {};
+
     if (title) {
       filter.product_name = new RegExp(title, "i");
       //   }
@@ -112,7 +96,7 @@ router.get("/offers", async (req, res) => {
 
     const numberOffersPerPage = 50;
     let numberSkip = 0;
-    if (page > 1) {
+    if (Number(page) > 1) {
       numberSkip = page * numberOffersPerPage;
     }
 
@@ -125,36 +109,37 @@ router.get("/offers", async (req, res) => {
     }
 
     const results = await Offer.find(filter)
-      .populate("owner")
+      .populate("owner", "account")
       .skip(numberSkip)
       .limit(numberOffersPerPage)
       .sort(sortPrice);
 
     const count = results.length;
-    const returnTab = [];
-    let offer = {};
-    // boucle sur le tableau pour mla mise en forme
-    for (let i = 0; i <= results.length - 1; i++) {
-      //mise en forme offer retour
-      offer = {
-        _id: results[i].id,
-        product_name: results[i].product_name,
-        product_description: results[i].product_description,
-        product_price: results[i].product_price,
-        product_details: results[i].product_details,
-        owner: results[i].owner.account,
-        product_image: {
-          // ...
-          // informations sur l'image du produit
-          secure_url: results[i].product_image.secure_url,
-          // ...
-        },
-      };
-      returnTab.push(offer);
-    }
+
+    // const returnTab = [];
+    // let offer = {};
+    // // boucle sur le tableau pour mla mise en forme
+    // for (let i = 0; i <= results.length - 1; i++) {
+    //   //mise en forme offer retour
+    //   offer = {
+    //     _id: results[i].id,
+    //     product_name: results[i].product_name,
+    //     product_description: results[i].product_description,
+    //     product_price: results[i].product_price,
+    //     product_details: results[i].product_details,
+    //     owner: results[i].owner.account,
+    //     product_image: {
+    //       // ...
+    //       // informations sur l'image du produit
+    //       secure_url: results[i].product_image.secure_url,
+    //       // ...
+    //     },
+    //   };
+    //   returnTab.push(offer);
+    // }
 
     //console.log(result);
-    res.status(200).json({ count: count, offers: returnTab });
+    res.status(200).json({ count: count, offers: results });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -163,7 +148,8 @@ router.get("/offers", async (req, res) => {
 router.get("/offer/:id", async (req, res) => {
   try {
     const offer = await Offer.findById({ _id: req.params.id }).populate(
-      "owner"
+      "owner",
+      "account"
     );
     res.status(200).json(offer);
   } catch (error) {
